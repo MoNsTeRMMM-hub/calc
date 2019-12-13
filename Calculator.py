5b9677c13b5bd834adc23bfcb1e9c1bcd35ed21e
MoNsTeRMMM-hub <58849994+MoNsTeRMMM-hub@users.noreply.github.com>
2019-12-13 15:54:44 +0200

    Calculator.
    None of anyone's busieness

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
-current_year = int(input("what's the year now?"))
-year_of_birth = int(input("what's the year of born?"))
+current_year = int(input("enter the current year now: "))
+year_of_birth = int(input("enter the year of birth: "))
 age = current_year - year_of_birth
 agestr = str(age)
 
-print("you are " + agestr + "old")
+print("you are " + agestr + " old")
